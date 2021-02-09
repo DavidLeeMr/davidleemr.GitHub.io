9dbd8b0e4391323d1f3d383d98151789b9dcc615
DavidLeeMr <49365040+DavidLeeMr@users.noreply.github.com>
2021-02-09 14:01:47 +0800

    Update head.js

--- template/js/head.js
+++ template/js/head.js
@@ -6,7 +6,7 @@ document.writeln("<body>");
 document.writeln("<div class=\'navbar\'>");
 document.writeln("	<a href=\'#\'><img src=\'icon.png\' width=\'15\'/></a>");
 document.writeln("	<a href=\'/index.html\'>首页</a>");
-document.writeln("	<a href=\'https://bbs.cceclub.org\'>论坛</a>");
+document.writeln("	<a href=\'http://bbs.cceclub.org\'>论坛</a>");
 document.writeln("	<a href=\'/note.html\'>笔记</a>");
 document.writeln("	<a href=\'https://cceclub.org\' class=\'right\'>#</a>");
 document.writeln("</div>");
